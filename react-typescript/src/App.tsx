51305f53d16deb8ba33a34169eb04d16d0616a1e
Umair513 <soomroumair513@gmail.com>
2023-07-19 11:50:08 +0500

    useContext

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -9,13 +9,19 @@ import Oscar from './components/Oscar';
 import Person from './components/Person';
 import PersonList from './components/PersonList';
 import Status from './components/Status';
+import { ThemeContextProvider } from './components/context/ThemeContext';
+import Box from './components/context/Box';
+import { UserContextProvider } from './components/context/UserContext';
+import  User  from "./components/context/User"
 
 function App() {
   
   
   return (
     <div className="App">
-      <Container styles={{border:"1px solid red", padding:"1rem"}}></Container>
+      <UserContextProvider>
+        <User></User>
+     </UserContextProvider>
     </div>
   );
 }
